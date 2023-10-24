78ef15e624782ad98205aad81cab87fcf0d49727
pmp59100 <leopoldpetit59@gmail.com>
2023-10-24 22:17:14 +0200

    Initial commit

---
+++ app/src/main/java/info/b3/q1/medictime/MainActivity.kt
@@ -0,0 +1,11 @@
+package info.b3.q1.medictime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_prises_page)
+    }
+}
